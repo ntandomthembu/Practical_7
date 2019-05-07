6dc2cdf11748c870c66110af04060cb1da6109e2
tapiwarufaro <50359809+tapiwarufaro@users.noreply.github.com>
2019-05-07 11:48:53 +0200

    Update Site.js
    awehhhhhhhh, klin

---
+++ Site.js
@@ -0,0 +1,5 @@
+function myFunctionRufy()
+{
+  alert ("This is why i am excited for INF 272/354, i get to git GIT, get it? lol");
+               document.write ("This is why i am excited for INF 272/354, i get to git GIT, get it? lol");
+}
